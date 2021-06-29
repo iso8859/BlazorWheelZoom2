7030c407e0689617a0df5dd396d7a37e43141486
Remi THOMAS <remi@xtware.com>
2021-06-29 17:30:30 +0200

    Manage visible rectangle

--- BlazorSimpleSVG/SVGObject.cs
+++ BlazorSimpleSVG/SVGObject.cs
@@ -12,11 +12,57 @@ namespace BlazorSimpleSVG
     {
         static public string ToStringInvariant(this double val) => val.ToString(CultureInfo.InvariantCulture);
     }
+    public class rect
+    {
+        public double? left, top, right, bottom;
+
+        public rect()
+        {
+                
+        }
+
+        public rect(double _left, double _top, double _right, double _bottom)
+        {
+            left = _left;
+            top = _top;
+            right = _right;
+            bottom = _bottom;
+        }
+
+        public bool IsEmpty() => !left.HasValue || !top.HasValue || !right.HasValue || !bottom.HasValue;
+        public double width
+        {
+            get => right.Value - left.Value;
+            set => right = left.Value + value;
+        }
+
+        public double height
+        {
+            get => bottom.Value - top.Value;
+            set => bottom = top.Value + value;
+        }
+
+        public static rect Intersect(rect a, rect b)
+        {
+            double x1 = Math.Max(a.left.Value, b.top.Value);
+            double x2 = Math.Min(a.left.Value + a.width, b.left.Value + b.width);
+            double y1 = Math.Max(a.top.Value, b.top.Value);
+            double y2 = Math.Min(a.top.Value + a.height, b.top.Value + b.height);
+
+            if (x2 >= x1 && y2 >= y1)
+            {
+                return new rect(x1, y1, x2 - x1, y2 - y1);
+            }
+
+            return new rect();
+        }
+    }
     public class SVGContext
     {
         public string clip_name = "clip-path";
         public double x_offset = 0, y_offset = 0, zoom = 1;
-
+        public rect viewSize; // Size of the visible part zoom 1
+        public rect areaSize; // Size of the drawing part zoom 1
         public double Clip(double i, double minOffset)
         {
             return Math.Min(0, Math.Max(i, minOffset));
@@ -34,6 +80,11 @@ namespace BlazorSimpleSVG
         public string TranslateY(double y) => (y_offset + (y * zoom)).ToStringInvariant();
         public double Size(double i) => i * zoom;
         public string Size_s(double i) => (i * zoom).ToStringInvariant();
+        public void EnsureIsVisible(rect area)
+        {
+            x_offset = Clip(Size(-area.left.Value+10), viewSize.width - Size(areaSize.width));
+            y_offset = Clip(Size(-area.top.Value+10), viewSize.height - Size(areaSize.height));
+        }
     }
 
     public class SVGObject
@@ -44,6 +95,8 @@ namespace BlazorSimpleSVG
         static public readonly string _fillopacity = "1";
 
         public string id;
+        public rect rect = new rect();
+        public bool scrollIntoView = false;
         public bool visible = true;
         public virtual string GetSVG(SVGContext context) { return ""; }
 
@@ -58,14 +111,12 @@ namespace BlazorSimpleSVG
 
     public class SVGRectangle : SVGObject
     {
-        public double left, top, right, bottom;
         public string color = _black;
         public string fill = _none;
         public string fill_opacity = _fillopacity;
-
         public override string GetSVG(SVGContext context)
         {
-            var tmp = $"<rect {GetId()} x='{context.TranslateX(left)}' y='{context.TranslateY(top)}' width='{context.Size_s(right - left)}' height='{context.Size_s(bottom - top)}' fill='{fill}' fill-opacity='{fill_opacity}' stroke='{color}' stroke-width='1'/>";
+            var tmp = $"<rect {GetId()} x='{context.TranslateX(rect.left.Value)}' y='{context.TranslateY(rect.top.Value)}' width='{context.Size_s(rect.width)}' height='{context.Size_s(rect.height)}' fill='{fill}' fill-opacity='{fill_opacity}' stroke='{color}' stroke-width='1'/>";
             //Console.WriteLine(tmp);
             return tmp;
         }
@@ -78,16 +129,19 @@ namespace BlazorSimpleSVG
 
     public class SVGImage : SVGObject
     {
-        public double left, top, right = -1, bottom = -1;
         public string href;
 
         public override string GetSVG(SVGContext context)
         {
-            var tmp = $"<image {GetId()} xlink:href='{href}' x='{context.TranslateX(left)}' y='{context.TranslateY(top)}' ";
-            if (right != -1)
-                tmp += $"width='{context.Size_s(right - left)}' ";
-            if (bottom != -1)
-                tmp += $"height='{context.Size_s(bottom - top)}'";
+            var tmp = $"<image {GetId()} xlink:href='{href}' ";
+            if (rect.left.HasValue)
+                tmp += $"x='{context.TranslateX(rect.left.Value)}' "; 
+            if (rect.top.HasValue)
+                tmp += $"y='{context.TranslateY(rect.top.Value)}' ";
+            if (rect.left.HasValue && rect.right.HasValue)
+                tmp += $"width='{context.Size_s(rect.width)}' ";
+            if (rect.top.HasValue && rect.bottom.HasValue)
+                tmp += $"height='{context.Size_s(rect.height)}'";
             tmp += "/>";
             //Console.WriteLine(tmp);
             return tmp;
