73e51b3be5dcfc18144178c153dc010d61ee2e00
Rémi THOMAS <remi@xtware.com>
2025-10-09 16:07:38 +0200

    Divers corrections plantage + traces banque de document

--- BlazorWheelZoom2/wwwroot/scripts.js
+++ BlazorWheelZoom2/wwwroot/scripts.js
@@ -1,27 +1,34 @@
+export function checkValidElement(el_id) {
+    return document.getElementById(el_id) !== null;
+}
 export function capturePointer(el, p) {
-    el.setPointerCapture(p);
+    if (checkValidElement(el.id)) {
+        el.setPointerCapture(p);
+    }
 }
 
 export function releasePointer(el, p) {
-    el.releasePointerCapture(p);
+    if (checkValidElement(el.id)) {
+        el.releasePointerCapture(p);
+    }
 }
 
 export function GetBoundingClientRect(el_id) {
-    if (document.getElementById(el_id) !== null)
+    if (checkValidElement(el_id))
         return JSON.stringify(document.getElementById(el_id).getBoundingClientRect());
     else
         return '{"x":0,"y":0,"width":0,"height":0,"top":0,"right":0,"bottom":0,"left":0}';
 }
 
 export function GetElementWidth(elid) {
-    if (document.getElementById(elid) !== null)
+    if (checkValidElement(elid))
         return document.getElementById(elid).getBoundingClientRect().width;
     else
         return 0;
 }
 
 export function GetElementHeight(elid) {
-    if (document.getElementById(elid) !== null)
+    if (checkValidElement(elid))
         return document.getElementById(elid).getBoundingClientRect().height;
     else
         return 0;
